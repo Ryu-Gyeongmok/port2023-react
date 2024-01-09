c45b433934e48f0c576c634ad736877937754302
에이치앤웍스/류경목-연구소정보시스템팀/박창건 <H2002034@hkmc.hkmg.global>
2024-01-09 16:32:21 +0900

    인트로까지 커밋!!!!

---
+++ src/components/Intro.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import HDC from "../assets/img/HDC.jpg";
+
+const Intro = () => {
+    return (
+        <section id="intro">
+            <div className="intro__inner">
+                <h1 className="intro__title">{introText.title}</h1>
+                <div className="intro__lines" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+                <div className="intro__text">
+                    <div className="text">
+                        <div>{introText.desc[0]}</div>
+                        <div>{introText.desc[1]}</div>
+                        <div>{introText.desc[2]}</div>
+                    </div>
+                    <div className="img">
+                        <img src={HDC} alt="어바웃" />
+                    </div>
+                </div>
+                <div className="intro__lines bottom" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+const introText = {
+    title: "Test Title",
+    desc: ["H: HAE", "D: DAE", "C: CHUL"],
+};
+
+export default Intro;
